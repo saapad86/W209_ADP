c3e81c3d1d50af74ef1e7988291938c725dc7746
aminvenjara <amin.venjara@gmail.com>
2016-08-08 23:16:32 -0400

    Changes to Jobs Explorer Formatting
    - Chaned Zoom buttons, updated default
    - added 'K' to axis labels
    - added 'K' to tooltip format and changed Year format
    - removed Company Size button from HTML

--- flask1/static/js/jobs-explorer-region.js
+++ flask1/static/js/jobs-explorer-region.js
@@ -4,32 +4,68 @@ $(function() {
             renderTo: 'container'
         },
         rangeSelector: {
-            selected: 5    // refers to the index of button array that is pre-selected, see details here: http://api.highcharts.com/highstock#rangeSelector.buttons
+            buttons: [{
+                type: 'month',
+                count: 6,
+                text: '6m'
+            }, {
+                type: 'year',
+                count: 1,
+                text: '1y'
+            }, {
+                type: 'year',
+                count: 5,
+                text: '5y'
+            }, {
+                type: 'ytd',
+                text: 'YTD'
+            }, {
+                type: 'all',
+                text: 'All'
+
+            }],
+            selected: 2    // refers to the index of button array that is pre-selected, see details here: http://api.highcharts.com/highstock#rangeSelector.buttons
         },
+        
         legend: {
             enabled: true,
             maxHeight: 100               
         },
+        
+        xAxis: {
+            dateTimeLabelFormats: {
+                millisecond:"%A, %b %e, %H:%M:%S.%L",
+                second:"%A, %b %e, %H:%M:%S",
+                minute:"%A, %b %e, %H:%M",
+                hour:"%A, %b %e, %H:%M",
+                day:"%A, %b %e, %Y",
+                week:"Week from %A, %b %e, %Y",
+                month:"%b %Y",
+                year:"%Y"
+            },
+        },
+        
         yAxis: {
             labels: {
                 formatter: function () {
-                    return (this.value > 0 ? ' + ' : '') + this.value;
+                    return (this.value > 0 ? ' + ' : '') + this.value + 'K';
                 }
             },
         },
+        
         tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> <br/>',
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}K</b> <br/>',
             valueDecimals:0,
             dateTimeLabelFormats: {
-                millisecond: '%H:%M:%S.%L',
-                second: '%H:%M:%S',
-                minute: '%H:%M',
-                hour: '%H:%M',
-                day: '%b %Y',
-                week: '%e. %b',
-                month: '%b \'%y',
-                year: '%Y'
-            }
+                millisecond:"%A, %b %e, %H:%M:%S.%L",
+                second:"%A, %b %e, %H:%M:%S",
+                minute:"%A, %b %e, %H:%M",
+                hour:"%A, %b %e, %H:%M",
+                day:"%A, %b %e, %Y",
+                week:"Week from %A, %b %e, %Y",
+                month:"%B %Y",
+                year:"%Y"
+            },
         },
         credits: {
             enabled: false
